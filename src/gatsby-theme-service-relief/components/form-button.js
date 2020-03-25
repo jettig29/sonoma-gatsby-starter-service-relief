cfc552f0b15b5a91d22bf43777f2a72779934ebe
Dustin Schau <dustinschau@gmail.com>
2020-03-24 21:46:59 -0700

    chore: fix and update

---
+++ src/gatsby-theme-service-relief/components/form-button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+export default () => (
+  <Link
+    to="/submit"
+    className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+  >
+    Add your local service &rarr;
+  </Link>
+)
